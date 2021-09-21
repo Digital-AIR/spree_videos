e2c815f54647784f12019c5f0cb86038f2da4a78
adhik.mehta@digitalairtech.com <adhik.mehta@digitalairtech.com>
2021-09-21 17:53:56 +0530

    Added Video Tab

--- app/overrides/_product.rb
+++
@@ -1,8 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/sub_menu/_product',
-  name: 'Add videos sub menu',
-  insert_bottom: '[data-hook="admin_product_sub_tabs"]',
-  text: <<-HTML
-        <%= tab :videos, match_path: '/videos' %>
-    HTML
-    )
